c73317f0c21ae06b3ba34edc028f13668b0e93fd
GeekEast <sx.ba2017@gmail.com>
2019-12-13 18:02:18 +1030

    toc

---
+++ Project5/webpack.config.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
